22e5df76a92b8150be813f5179bb39919324604d
Sebastian <sebastianhereu@gmail.com>
2022-03-26 02:20:52 +0000

    almost done

--- client_chat.py
+++ client_chat.py
@@ -7,7 +7,6 @@ import signal
 import time
 
 
-#accept_msg = 'Client table updated.'
 ACK_recvd = True
 ERR_recvd = False
 listening = True
@@ -18,9 +17,14 @@ friend_ip_map = dict()
 lock = threading.Semaphore(1)
 server_dest = ('0.0.0.0', 0)
 my_name = ''
-
+accept_msg = 'Client table updated.'
 
 def send_ACK(socket, addr, use_name = False, name = ''):
+    '''
+    Sends specially formatted ACK. MAGIC_NUM is defined in util.py and is used for special
+    communication between client and server.
+    :return: None
+    '''
     if not use_name:
         util.Send(socket, str(util.MAGIC_NUM).encode(), addr)
     else:
@@ -30,34 +34,16 @@ def send_ACK(socket, addr, use_name = False, name = ''):
 def timeout_handler(signum, frame):
     raise util.timoutexception('timout!')
 
-def wait_ACK_h(local_SC):
-    global ACK_SC
-    while(local_SC == ACK_SC):
-        pass
-
-
 def wait_ACK(send_addr, local_SC, client = True, duration = 0.5):
+    '''
+    Sleeps for duration seconds and checks if an ACK has bee recieved to the listening thread.
+    :return: Boolean indicating whether an ACK was recieved.
+    '''
     global ACK_recvd
     global ACK_SC
     name = ''
     if client:
         name = friend_ip_map[(send_addr[0], send_addr[1])][0]
-#    signal.alarm(1)
-#    try:
-#        #signal.setitimer(signal.ITIMER_REAL, 0.5)
-#        wait_ACK_h(local_SC)
-#        #signal.setitimer(signal.ITIMER_REAL, 0)
-#        signal.alarm(0)
-#        util.pmessage('Message recieved by ' + name +'.')
-#        return True
-#    except Exception as e:
-#        if client:
-#            friend_ip_map[(send_addr[0], send_addr[1])] = (name ,False)
-#            util.pmessage(' No ACK from ' + name + ' ,message sent to server.')
-#        #signal.setitimer(signal.ITIMER_REAL, 0)
-#        signal.alarm(0)
-#        return False
-
     time.sleep(duration)
     if(local_SC == ACK_SC):
         if client:
@@ -70,11 +56,19 @@ def wait_ACK(send_addr, local_SC, client = True, duration = 0.5):
         return True
 
 def notify_leave(sock, name, addr):
+    '''
+    sends specially formatted message to server indicating that client wants to deregister.
+    :return:None
+    '''
     s = 'dereg ' + name
     util.Send(sock, s.encode(), addr)
 
 
 def notify_server_leave(sock, name, send_addr, local_SC):
+    '''
+    tries to deregister 6 times (inital try + five retries). Has to wait for ack each time.
+    :return: boolean indicating whether server ackd client's deregistration
+    '''
     count = 0
     status = False
     while(count < 6 and (not status)):
@@ -84,6 +78,10 @@ def notify_server_leave(sock, name, send_addr, local_SC):
     return status
 
 def notify_server_channel_msg(sock, message, send_addr, local_SC):
+    '''
+    tries to send channel message 6 times (inital try + five retries). Has to wait for ack each time.
+    :return: boolean indicating whether server ACKd client's channel message
+    '''
     count = 0
     status = False
     while(count < 6 and (not status)):
@@ -97,6 +95,11 @@ def notify_server_channel_msg(sock, message, send_addr, local_SC):
     return status
 
 def wait_ACK_ERR(local_SC):
+    '''
+    Pertains to savemessages. Wait either for ACK of savemessage, error (indicating that client is actually online),
+    or server not responding 
+    :return: -1,0,1 indicating status of savemessage
+    '''
     global ERR_recvd
     time.sleep(0.5)
     evalu = time.time()
@@ -108,26 +111,33 @@ def wait_ACK_ERR(local_SC):
         return -1
 
 def send_save_message(sock, name, message, local_SC):
-        global server_dest
-        global ERR_recvd
-        global lock
-        global ACK_SC
-
-        s = 'sendsave ' + name + ' ' + message
-        lock.acquire()
-        local_SC = ACK_SC
-        ERR_recvd = False
-        lock.release()
-        time.sleep(0.5)
-        util.Send(sock, s.encode(), server_dest)
-        ret = wait_ACK_ERR(local_SC)
-        if ret == 1:
-            util.pmessage('Messages recieved by the server and saved')
-        elif ret == -1:
-            util.pmessage('Client ' + name + ' exists!!')
-        else:
-            util.pmessage('Sendsave message failed. Try again.')
+    '''
+    Attempts to send savemessage. Uses wait_ACK_err for status of savemessage.
+    :return: None
+    '''    
+    global server_dest
+    global ERR_recvd
+    global lock
+    global ACK_SC
+    s = 'sendsave ' + name + ' ' + message
+    lock.acquire()
+    local_SC = ACK_SC
+    ERR_recvd = False
+    lock.release()
+    time.sleep(0.5)
+    util.Send(sock, s.encode(), server_dest)
+    ret = wait_ACK_ERR(local_SC)
+    if ret == 1:
+        util.pmessage('Messages recieved by the server and saved')
+    elif ret == -1:
+        util.pmessage('Client ' + name + ' exists!!')
+    else:
+        util.pmessage('Sendsave message failed. Try again.')
 def clnt_setup():
+    '''
+    Client setup. Binds client to specified port given via CLA.
+    :return: working UDP socket for client
+    '''
     my_port = util.Port(sys.argv[4]) 
     clnt_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM); 
     clnt_sock.bind(('0.0.0.0', my_port))
@@ -135,24 +145,30 @@ def clnt_setup():
 
 
 def reg_to_server(sock, name, addr):
+    '''
+    Uses MAGIC_NUM to register with server after deregistration
+    :return: None
+    '''
     s = str(util.MAGIC_NUM) + 'R' + ' ' + name
     send_s = s.encode()
     util.Send(sock, send_s, addr)
 
 def display_mail(messages):
+    '''
+    Displays mail sent to offline client when they log back in via registering.
+    :return: None
+    '''
     util.pmessage('YOU HAVE MESSAGES')
     lines = messages.split('\n') 
     for line in lines:
         if line.strip() != '':
             util.pmessage(line + '\n', False)
 
-#def add_client(address, port, name)::
-#    online_status = True
-#    client_ips[address] = 1
-#    client_map[(address, port)] = (name, online_status)
-#    util.pmessage(accept_msg)
-#
 def clnt_send_h(sock, inp, dest):
+        '''
+        Helper function to main sending loop. Reads client input and dispatches accordingly
+        :return: None
+        '''
         global friend_map
         global friend_ip_map
         global server_dest
@@ -163,12 +179,16 @@ def clnt_send_h(sock, inp, dest):
         split_inp = inp.split()
         if(split_inp[0] == 'send' or split_inp[0] == 'send_all'):
             isbroadcast = split_inp[0] == 'send_all'
+            if isbroadcast and len(split_inp) == 1:
+                return
+            if not isbroadcast and len(split_inp) <= 2:
+                return
             message = ('send_all ' + ' '.join(split_inp[1:])).encode() if isbroadcast else ' '.join(split_inp[2:]).encode()
             name = None if isbroadcast else split_inp[1]
-            info = None if isbroadcast else friend_map[name]
             if not isbroadcast and name not in friend_map.keys(): 
                 util.pmessage('error: invalid name {0}'.format(name), False)
                 return
+            info = None if isbroadcast else friend_map[name]
             if(isbroadcast or info[2]):
                 lock.acquire()
                 ACK_recvd = False
@@ -190,11 +210,14 @@ def clnt_send_h(sock, inp, dest):
                 lock.release()
                 send_save_message(sock, name, message.decode(), local_SC)
         
-        elif(split_inp[0] == 'dereg'):
+        elif(len(split_inp) == 2 and split_inp[0] == 'dereg'):
+            name = split_inp[1]
             lock.acquire()
-            local_SC = ACK_SC
+            local_SC = ACK_SC   #capture global ACK state to wait for ACK in notify_server_leave
             lock.release()
             listening = False
+            if name not in friend_ip_map.keys() and name != my_name:
+                return
             if notify_server_leave(sock, split_inp[1],server_dest, local_SC):
                 util.pmessage('You are Offline. Bye')
             else:
@@ -202,62 +225,85 @@ def clnt_send_h(sock, inp, dest):
                 util.pmessage('Exiting')
         elif(split_inp[0] == 'reg' and len(split_inp) == 2):
             name = split_inp[1]
+            if name not in friend_ip_map.keys() and name != my_name:
+                return
             reg_to_server(sock, name, server_dest)
             listening = True
 
 
 def clnt_send(sock, dest):
+    '''
+    Main loop for sending thread of client. Waits for input from client on infinite loop.
+    :return: None
+    '''
     while(True):
         util.pmessage('', False)
         inp = input()
         clnt_send_h(sock, inp, dest)
 
-def update_table(fields):
+def update_table(fields, display= False):
+    '''
+    Updates global friend_table w/ information sent by server.
+    :return: None
+    '''
     global friend_map
     global friend_id_map
+    global accept_msg
     friend_map[fields[2]] = (fields[0], int(fields[1]), bool(int(fields[3])))
     friend_ip_map[(fields[0], int(fields[1]))] = (fields[2], bool(int(fields[3])))
-
-
+    if display:
+        util.pmessage(accept_msg)
 
 def clnt_listen(sock):
+    '''
+    Main function of listening thread. Reads from socket and interprets messages from server and accounts for updates global ACK state.  
+    :return: None
+    '''
     global ACK_recvd
     global ACK_SC
     global server_dest
     global ERR_recvd
     global listening
+    global my_name
     while(True):
         sender_message, sender_address = sock.recvfrom(util.SIZE)
         sender_message = sender_message.decode('UTF-8')
-        if(sender_message == str(util.MAGIC_NUM)):
+        if(sender_message == str(util.MAGIC_NUM)): #we recieved an ACK
             lock.acquire()
             ACK_recvd = True
             ACK_SC+=1
             update = time.time()
-       #     print('ACK UPDATED ',ACK_SC , ' ', update)
             lock.release()
             continue
-        if(sender_message == str(util.MAGIC_NUM)+'E'):
+        if(sender_message == str(util.MAGIC_NUM)+'E'): #we recieved an error for savesend message
             lock.acquire()
             ERR_recvd = True
             lock.release()
             continue
         split_message = sender_message.split() 
-        if split_message[0] == 'TU':
-            update_table(split_message[1:])
-        elif sender_message == str(util.MAGIC_NUM) + 'O':
+        if split_message[0] == 'TU': #TU, TUW, TUF are all updates on friend table
+            update_table(split_message[1:], True)
+        elif split_message[0] == 'TUW':
+            update_table(split_message[1:], False)
+        elif split_message[0] == 'TUF':
+            update_table(split_message[1:], False)
+        elif sender_message == str(util.MAGIC_NUM) + 'O' and listening:
             send_ACK(sock, server_dest)
         elif split_message[0] == 'MAIL':
-            display_mail(sender_message) #display the mail to the client 
+            display_mail(sender_message) #display the mail to the client from server once registering
         elif split_message[0] == 'Channel_Message' and listening:
             send_ACK(sock, server_dest, True, my_name)
             util.pmessage(sender_message)
-        elif listening:
+        elif listening: # if we get here we know that this is a message
             name = friend_ip_map[sender_address][0] + ': ' if sender_address != server_dest else ''
             send_ACK(sock, sender_address)
             util.pmessage(name +  sender_message)
 
 def main():
+    '''
+    Main function : performs setup, intial registration w/ server, starts lisetning and sending threads
+    :return: None
+    '''
     global server_dest
     global my_name
     if len(sys.argv) != 5:
@@ -266,9 +312,9 @@ def main():
     server_dest = ( sys.argv[2], util.Port(sys.argv[3]))
     my_name = sys.argv[1]
     my_sock = clnt_setup()
-    util.Send(my_sock, str.encode(my_name), server_dest)
+    util.Send(my_sock, str.encode(my_name), server_dest) #initial connection w/ server, send nick name
 
-    send_thread = threading.Thread(target = clnt_send, args = (my_sock, server_dest))
+    send_thread = threading.Thread(target = clnt_send, args = (my_sock, server_dest)) 
     listen_thread = threading.Thread(target = clnt_listen, args = (my_sock, ))
 
     send_thread.start()
--- server_chat.py
+++ server_chat.py
@@ -14,36 +14,33 @@ client_map = dict()
 listening = False
 name_set = set()
 server_sock = None
+ACK_SC = 0
+
 
 def timeout_handler(signum, frame):
     raise util.TimoutException('timout!')
 
 def send_ACK(socket, addr):
+    '''
+    Sends specially formatted ACK w/ MAGIC_NUM
+    :return: None
+    '''
     util.Send(socket, str(util.MAGIC_NUM).encode(), addr)
 
-#def wait_ACK(send_addr, local_SC, client = True):
-#    global ACK_recvd
-#    global ACK_SC
-#    name = ''
-#    if client:
-#        name = friend_ip_map[(send_addr[0], send_addr[1])][0]
-#    time.sleep(0.5)
-#    if(local_SC == ACK_SC):
-#        if client:
-#            friend_ip_map[(send_addr[0], send_addr[1])] = (name ,False)
-#            util.pmessage(' No ACK from ' + name + ' ,message sent to server.')
-#        return False
-#    else:
-#        if client:
-#            util.pmessage('Message recieved by ' + name + '.')
-#        return True
-#
 def send_ERR(socket, addr):
+    '''
+    Sends specially formatted ERR msg w/ MAGIC_NUM
+    :return: None
+    '''
     s = str(util.MAGIC_NUM) + 'E'
     send_s = s.encode()
     util.Send(socket, send_s, addr)
 
 def server_setup():
+    '''
+    Performs inital server setup. Binds to port.
+    :return: working server socket
+    '''
     port = util.Port(sys.argv[1])
     server_sock = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     try:
@@ -54,35 +51,35 @@ def server_setup():
     return server_sock
 
 def broadcast_update(socket, u_address, u_port, u_name, u_online_status):
+    '''
+    Broadcasts table update to all registered clients
+    :return: None
+    '''
     s = 'TU ' + u_address + ' ' + str(u_port) + ' ' + u_name + ' ' + str(int(u_online_status))
     send_s = str.encode(s)
     for dest in client_map.keys():
         util.Send(socket,send_s,dest)
 
 def broadcast_message(socket, sender_name, message):
+    '''
+    Sends Channel Message to all clients. If a client does not respond with ACK after 0.5s, they are set to offline
+    and the message gets saved in their file.
+    :return: None
+    '''
     global listening
     global name_set
+    global accept_msg
     for dest in client_map.keys():
         s = 'Channel_Message ' + sender_name + ': ' + message 
         sender_dest = (client_ips_map[sender_name][0], client_ips_map[sender_name][1])
         if sender_dest != dest:
-            #if client_ips_map[sender_name][2]:
-            #if client_map[dest][1]:
             send_s = s.encode()
             util.Send(socket, send_s, dest)
-           # else:
-           #         p = client_map[dest][0]
-           #         if os.path.exists(p):
-           #             f = open(p, 'a')
-           #         else:
-           #             f = open(p, 'w')
-           #         f.write(s + '\n')
-           #         f.close()
-    listen_thread = threading.Thread(target = server_listen, args = (server_sock, True)) 
+    listen_thread = threading.Thread(target = server_listen, args = (server_sock, True)) #start a concurrent lisening thread to wait on ACKs 
     listening = True
-    name_set = set(client_ips_map.keys())
-    name_set.remove(sender_name)
-    listen_thread.start()
+    name_set = set(client_ips_map.keys()) # set global name_set to be full list of clients
+    name_set.remove(sender_name) # remove sender from list of clients
+    listen_thread.start() #start ACK-waiting thread
     time.sleep(0.5)
     listening = False
     for name in name_set:
@@ -93,17 +90,42 @@ def broadcast_message(socket, sender_name, message):
             f = open(p, 'w')
         f.write(s + '\n')
         f.close()
+        if client_ips_map[name][2]: 
+            addr = client_ips_map[name][0]
+            port = client_ips_map[name][1]
+            client_ips_map[name] = (addr, port, False)
+            client_map[(addr, port)] = (name , False)
+            broadcast_update(server_sock, addr, port, name, False)
 
     listen_thread.join()
 
 
 def send_table(socket, dest):
+    '''
+    Send the entire table to a client. TUW and TUF prefixes are for bookkeeping at client side.
+    :return: None
+    '''
+    count = len(client_map.keys()) - 1
+    if count == -1:
+        return
+    k = None
     for client in client_map.keys():
-        s = 'TU ' + client[0] + ' ' + str(client[1]) + ' ' + client_map[client][0] + ' ' + str(int(client_map[client][1]))
+        if count == 0:
+            k = client
+            break;
+        count-=1
+        s = 'TUW ' + client[0] + ' ' + str(client[1]) + ' ' + client_map[client][0] + ' ' + str(int(client_map[client][1]))
         send_s = str.encode(s)
         util.Send(socket,send_s,dest)
+    s = 'TUF ' + k[0] + ' ' + str(k[1]) + ' ' + client_map[k][0] + ' ' + str(int(client_map[k][1]))
+    send_s = str.encode(s)
+    util.Send(socket,send_s,dest)
 
 def send_mail(sock, name, addr):
+    '''
+    Opens file associated with name and sends the entire contents to client.
+    :return: None
+    '''
     s = 'MAIL '
     clnt_addr = client_ips_map[name][0]
     clnt_port = client_ips_map[name][1]
@@ -117,41 +139,52 @@ def send_mail(sock, name, addr):
     os.remove(name)
 
 def add_client(address, port, name, online_status):
+    '''
+    Adds a client to global maps of clients for first time.
+    :return: None
+    '''
     client_ips[address] = 1
     client_map[(address, port)] = (name , online_status)
     client_ips_map[name] = (address,port, online_status)
-    util.pmessage(accept_msg)
         
-def process_req(message, address, port):
-    print('Recieved! ', message.decode())
-
-
-def serve_client(socket):
-    while(True):
-        message, addrp = socket.recvfrom(util.SIZE)
-        process_req(message, addrp[0], addrp[1])
-    sys.exit()
-
-def wait_online_ACK(sock):
-    try:
-        signal.setitimer(signal.ITIMER_REAL, 0.5)
-        sender_message, sender_address = sock.recvfrom(util.SIZE)
-        signal.setitimer(signal.ITIMER_REAL, 0)
+#def serve_client(socket):
+#    while(True):
+#        message, addrp = socket.recvfrom(util.SIZE)
+#        process_req(message, addrp[0], addrp[1])
+#    sys.exit()
+#
+def wait_online_ACK(sock, local_SC):
+    '''
+    Waits for ACK when server is checking if client is online.
+    :return: None
+    '''
+    global ACK_SC
+    time.sleep(0.3)
+    if ACK_SC != local_SC:
         return True
-    except util.TimoutException:
-        signal.setitimer(signal.ITIMER_REAL, 0)
-        return False
+    return False
 
 def clnt_online(sock, addr):
+    '''
+    Sends message to client at addr to see if they are online.
+    :return: None
+    '''
+    global ACK_SC
     s = str(util.MAGIC_NUM)+'O'
     send_s = s.encode()
     util.Send(sock, send_s, addr)
-    wait_online_ACK(sock)
+    local_SC = ACK_SC
+    return wait_online_ACK(sock, local_SC)
 
 
 def server_listen(server_sock, ACK = False):
+    '''
+    Main loop of server. Listens for incoming requests for sendsave messages, dereg/reg requests, and broadcast messages
+    :return: None
+    '''
     global listening
     global name_set
+    global ACK_SC
     condition = listening if ACK else True
     while(True): 
         sender_message, sender_address = server_sock.recvfrom(util.SIZE)
@@ -162,20 +195,21 @@ def server_listen(server_sock, ACK = False):
             add_client(sender_address[0], sender_address[1], sender_message, online_status)
             broadcast_update(server_sock, sender_address[0], sender_address[1], sender_message, online_status)
         split_message = sender_message.split()
-        if(len(split_message) == 2 and split_message[0] == 'dereg' and split_message[1] in client_ips_map.keys()):
+        if(len(split_message) == 2 and split_message[0] == 'dereg' and split_message[1] in client_ips_map.keys()): #dereg request
             name = split_message[1]
+            if name not in client_ips_map.keys():
+                return
             clnt_addr = client_ips_map[name][0]
             clnt_port = client_ips_map[name][1] 
             client_map[(clnt_addr, clnt_port)] = (name, False)
             client_ips_map[name] = (clnt_addr, clnt_port, False)
             send_ACK(server_sock, sender_address)
             broadcast_update(server_sock, client_ips_map[name][0], client_ips_map[name][1], name, False)
-            util.pmessage(accept_msg)
-        elif(len(split_message) >= 3 and split_message[0] == 'sendsave' and split_message[1] in client_ips_map.keys()):
+        elif(len(split_message) >= 3 and split_message[0] == 'sendsave' and split_message[1] in client_ips_map.keys()): #sendsave message. If client isnt online, save msg to thier file.
             name = split_message[1]
             sender_name = client_map[sender_address][0]
             clnt_addr = client_ips_map[name][0]
-            clnt_port = client_ips_map[name][1]
+            clnt_port = client_ips_map[name][1]  
             isonline = clnt_online(server_sock, (clnt_addr, clnt_port))
             if not client_ips_map[name][2] or not isonline:
                 send_ACK(server_sock, sender_address)
@@ -189,31 +223,36 @@ def server_listen(server_sock, ACK = False):
                 if not isonline and client_ips_map[name][2] != isonline:
                     client_ips_map[name] = (clnt_addr, clnt_port, False)
                     client_map[(clnt_addr, clnt_port)] = (name , False)
-                    broadcast_update(server_sock, clnt_addr, clnt_port, name, False)
-                    util.pmessage(accept_msg)
             else:
                 send_ERR(server_sock, sender_address)
                 broadcast_update(server_sock, clnt_addr, clnt_port, name, True)
-        elif(len(split_message) == 2 and split_message[0] == str(util.MAGIC_NUM) + 'R'):
+        elif(len(split_message) == 2 and split_message[0] == str(util.MAGIC_NUM) + 'R'): #request to register
             name = split_message[1]
+            if name not in client_ips_map.keys():
+                return
             clnt_addr = client_ips_map[name][0]
             clnt_port = client_ips_map[name][1]
             send_mail(server_sock, name, (clnt_addr, clnt_port))
             client_map[(clnt_addr, clnt_port)] = (name, True)
             client_ips_map[name] = (clnt_addr, clnt_port, True)
             broadcast_update(server_sock, clnt_addr, clnt_port, name, True)
-            util.pmessage(accept_msg)
-        elif len(split_message) >= 2 and split_message[0] == 'send_all':
+            send_table(server_sock, (clnt_addr, clnt_port)) 
+        elif len(split_message) >= 2 and split_message[0] == 'send_all': #broadcast message from client
             sender_name = client_map[sender_address][0]
             message = ' '.join(split_message[1:])
             send_ACK(server_sock, sender_address)
             broadcast_message(server_sock, sender_name, message)
-        elif len(split_message) == 2 and split_message[0] == str(util.MAGIC_NUM):
-            name = split_message[1]
+        elif len(split_message) == 2 and split_message[0] == str(util.MAGIC_NUM):# ACK from specific client. Once recieved client can be removed from set
+            name = split_message[1]                                              # of clients that ACK has not recvd from
             if name in name_set:
                 name_set.remove(name)
+        elif len(split_message) == 1 and split_message[0] == str(util.MAGIC_NUM): # regular ACK
+            ACK_SC+=1
 
 def main():
+    '''
+    Main function: performs server setup and starts listening
+    '''
     global server_sock
     if len(sys.argv) != 2:
         util.Die('usage: {0} -s <port>'.format(util.MAIN_P))
